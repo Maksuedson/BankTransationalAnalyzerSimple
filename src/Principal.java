bdf2caf794475267ab00d8b2463390fa430ae3c1
maksuedson.silva <maksuedson.silva@fornecedores.sicoob.com.br>
2022-02-10 12:59:14 -0300

    Project created

---
+++ src/Principal.java
@@ -0,0 +1,25 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class Principal {
+	private static final String RESOURCES = "./src/resources/extratos.txt";
+
+	public static void main(String[] args) throws IOException {
+
+		Path path = Paths.get(RESOURCES);
+		List<String>lines = Files.readAllLines(path);
+		double total = 0d;
+		
+		for (String line: lines) {
+			String[] columns = line.split(",");
+			double amount = Double.parseDouble(columns[1]);
+			total += amount;
+		}
+		
+		System.out.println("O total de todas as transações é: " + total);
+	}
+
+}
